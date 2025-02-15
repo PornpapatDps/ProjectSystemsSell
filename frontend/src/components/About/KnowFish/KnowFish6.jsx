1611f0db7313b1fe6970889b71b1b793fcc6a621
Pornpapat <pornpapat.d@ku.th>
2025-02-16 01:07:05 +0700

    Update KnowFish6.jsx

--- frontend/src/components/About/KnowFish/KnowFish6.jsx
+++ frontend/src/components/About/KnowFish/KnowFish6.jsx
@@ -2,7 +2,7 @@ import img1 from '../../../assets/SA/14.png';
 
 import React from 'react'
 
-const KnowFish = () => {
+const KnowFish6 = () => {
   return (
     <div>
        <div className="flex flex-col-3  md:flex-row items-center p-20 font-[Kanit]  justify-center bg-gray-200 dark:bg-gray-800 dark:text-white ">
@@ -17,15 +17,16 @@ const KnowFish = () => {
           <h1 className="text-7xl font-semibold mb-2">ปลาซิวฟ้า </h1>
           <p className="text-4xl mb-4">ชื่อวิทยาศาสตร์: Blue Rasbora - Rasbora pauciperforata</p>
           <p className="text-3xl mb-4">ถิ่นกำเนิด: ไทย, มาเลเซีย, อินโดนีเซีย</p>
-          <p className="text-3xl mb-4 font-bold">ลักษณะเด่น</p>
+          <p className="text-3xl mb-4 font-bold">ลักษณะเด่นปลาซิวฟ้า</p>
           <ul className="text-2xl">
-            <li>ขนาดตู้ปลา: 10 แกลลอนขึ้นไป </li>
-            <li>ค่า pH: 6.0-7.5</li>
-            <li>อาหาร: อาหารเม็ดขนาดเล็ก, ไรแดง, หนอนแดง</li>
-            <li>การเลี้ยงรวม: อยู่ร่วมกับปลานีออน, กุ้งแคระ</li>
+          
+            <li>ลำตัวสีฟ้าน้ำเงินสว่าง มีแถบเหลือง-เขียวจาง ๆ </li>
+            <li>ครีบใสหรือมีสีส้มจาง ๆ</li>
+            <li>เป็นปลาที่เลี้ยงง่ายและเข้ากับปลาชนิดอื่นได้ดี</li>
+            <li> </li>
           </ul>
           <br/>
-          <p className="text-3xl mb-4 font-bold">การเลี้ยงปลาซิวไบจิเต้</p>
+          <p className="text-3xl mb-4 font-bold">การเลี้ยงปลาซิวฟ้า</p>
           <ul className="text-2xl">
             <li>✔  ขนาดตู้ปลา: 10-15 แกลลอนขึ้นไป</li>
             <li>✔ อุณหภูมิ: 24-28°C</li>
@@ -39,4 +40,4 @@ const KnowFish = () => {
   )
 }
 
-export default KnowFish
+export default KnowFish6
