ee61cd5fd183c6e37da401deb2dd69f2519246b1
Pornpapat <pornpapat.d@ku.th>
2025-02-16 01:07:30 +0700

    Update Category4.jsx

--- frontend/src/components/Category/Category4.jsx
+++ frontend/src/components/Category/Category4.jsx
@@ -4,6 +4,7 @@ import Image2 from "../../assets/SA/SushiRed.png";
 import Image3 from "../../assets/SA/Yellow.png";
 import Image4 from "../../assets/SA/Red.png";
 import Button from "../Shared/Button";
+import { Link } from 'react-router-dom';
 const Category = () => {
   return (
     <div className='py-3'>
@@ -18,11 +19,12 @@ const Category = () => {
                 <p className='mb-[2px] text-white-700'>กุ้งแคระ</p>
                 <p className='text-2xl font-semibold mb-[2px]'>ซูชิแดง</p>
                 <p className='text-4xl xl:text-5xl font-bold opacity-20 mb-2'>Sushi Red </p>
+                <Link to="/KnowShrimp7">
                 <Button
                   text="Browser"
                   bgColor={"bg-primary"}
                   textColor={"text-white"}
-                />
+                /> </Link>
               </div>
             </div>
             <img src={Image2} alt="" className='w-[300px] h-[250px] absolute -right-1 lg:top-[40px]' />
@@ -36,11 +38,12 @@ const Category = () => {
                 <p className='mb-[2px] text-white'>กุ้งแคระ</p>
                 <p className='text-2xl font-semibold mb-[2px]'>ส้มเคลือบเหลือง</p>
                 <p className='text-4xl xl:text-5xl font-bold opacity-40 mb-2'>Orange<br/>Rili Shrimp</p>
+                <Link to="/KnowShrimp2">
                 <Button
                   text="Browser"
                   bgColor={"bg-white"}
                   textColor={"text-primary"}
-                />
+                /> </Link>
               </div>
             </div>
             <img src={Image3} alt="" className='w-[350px] h-[250px] absolute -right-50 lg:top-[50px] ' />
@@ -53,11 +56,12 @@ const Category = () => {
                 <p className='mb-[2px] text-white-400'>กุ้งแคระ</p>
                 <p className='text-2xl font-semibold mb-[2px]'>แดง</p>
                 <p className='text-4xl xl:text-5xl font-bold opacity-20 mb-2'>Red Cherry Shrimp</p>
+                <Link to="/KnowShrimp5">
                 <Button
                   text="Browser"
                   bgColor={"bg-white"}
                   textColor={"text-brandGreen"}
-                />
+                /> </Link>
               </div>
             </div>
             <img src={Image4} alt="" className='w-[350px] h-[300px] absolute -right-5 lg:buttom-[20px]' />
@@ -70,11 +74,12 @@ const Category = () => {
                 <p className='mb-[2px] text-white-400'>กุ้งแคระ</p>
                 <p className='text-2xl font-semibold mb-[2px]'>สโนว์บอล</p>
                 <p className='text-4xl xl:text-5xl font-bold opacity-20 mb-2'>Snowball Shrimp</p>
+                <Link to="/KnowShrimp4">
                 <Button
                   text="Browser"
                   bgColor={"bg-white"}
                   textColor={"text-primary"}
-                />
+                /> </Link>
               </div>
             </div>
             <img src={Image1} alt="" className='w-[300px] h-[250px] absolute -right-5 lg:top-[10px]' />
