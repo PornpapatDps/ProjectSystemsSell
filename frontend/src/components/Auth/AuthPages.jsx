081fe0c18e09a6a519df5862a586a5eef0a75763
Pornpapat <pornpapat.d@ku.th>
2025-02-23 19:47:56 +0700

    Update AuthPages.jsx

--- frontend/src/components/Auth/AuthPages.jsx
+++ frontend/src/components/Auth/AuthPages.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import  { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
+// eslint-disable-next-line react/prop-types
 const AuthPage = ({ isSignUp }) => {
     const [userDetails, setUserDetails] = useState({ username: "", password: "", confirmPassword: "" });
     const navigate = useNavigate();
